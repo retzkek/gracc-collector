4537546d7286d6b5b63a1e1e5445c07896fcd4f8
Kevin Retzke <kretzke@fnal.gov>
2016-10-17 17:14:44 -0500

    change StorageElementRecord data fields to ints

--- gracc/storageelementrecord.go
+++ gracc/storageelementrecord.go
@@ -7,12 +7,17 @@ import (
 )
 
 type StorageElementRecord struct {
-	XMLName   xml.Name  `xml:"StorageElementRecord"`
-	UniqueID  string    `xml:",omitempty"`
-	Timestamp time.Time `xml:",omitempty"`
-	Origin    origin    `xml:,omitempty"`
-	Fields    []field   `xml:",any"`
-	raw       []byte    `xml:"-"`
+	XMLName        xml.Name  `xml:"StorageElementRecord"`
+	UniqueID       string    `xml:",omitempty"`
+	Timestamp      time.Time `xml:",omitempty"`
+	TotalSpace     uint64    `xml:",omitempty"`
+	FreeSpace      uint64    `xml:",omitempty"`
+	UsedSpace      uint64    `xml:",omitempty"`
+	FileCount      uint64    `xml:",omitempty"`
+	FileCountLimit uint64    `xml:",omitempty"`
+	Origin         origin    `xml:,omitempty"`
+	Fields         []field   `xml:",any"`
+	raw            []byte    `xml:"-"`
 }
 
 func (ser *StorageElementRecord) ParseXML(xb []byte) error {
@@ -45,6 +50,23 @@ func (ser *StorageElementRecord) ToJSON(indent string) ([]byte, error) {
 	// Standard time instants
 	if !ser.Timestamp.IsZero() {
 		r["Timestamp"] = ser.Timestamp.Format(time.RFC3339)
+	}
+
+	// data fields
+	if ser.TotalSpace > 0 {
+		r["TotalSpace"] = ser.TotalSpace
+	}
+	if ser.FreeSpace > 0 {
+		r["FreeSpace"] = ser.FreeSpace
+	}
+	if ser.UsedSpace > 0 {
+		r["UsedSpace"] = ser.UsedSpace
+	}
+	if ser.FileCount > 0 {
+		r["FileCount"] = ser.FileCount
+	}
+	if ser.FileCountLimit > 0 {
+		r["FileCountLimit"] = ser.FileCountLimit
 	}
 
 	// flatten other fields
